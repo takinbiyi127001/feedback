91ebf3aa02b3e76164df34f7e3b89f22b7f7ad30
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-03-14 20:51:02 +1000

    Store form data in the database

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Review(models.Model):
+    user_name = models.CharField(max_length=50)
+    review_text = models.CharField(max_length=200)
+    rating = models.IntegerField()
