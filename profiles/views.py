5bd77780a3a01795e95469f41236fa14f6e0fa10
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-03-15 20:29:17 +1000

    Added form a FileField

--- profiles/views.py
+++ profiles/views.py
@@ -2,14 +2,35 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
 
+from  .forms import ProfileForm
+
 
 # Create your views here.
 
+def store_file(file):
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
+
+
 class CreateProfileView(View):
     def get(self, request):
-        return render(request, "profiles/create_profile.html")
+        form = ProfileForm()
+        context = {
+            "form": form
+        }
+        return render(request, "profiles/create_profile.html", context=context)
 
     # Search django upload files for reference
     def post(self, request):
-        print(request.FILES['image'])
-        return HttpResponseRedirect("/profiles")
+        submitted_form = ProfileForm(request.POST, request.FILES)
+
+        if submitted_form.is_valid():
+            store_file(request.FILES['image'])
+            return HttpResponseRedirect("/profiles")
+
+        context = {
+            "form": submitted_form
+        }
+        return render(request, "profiles/create_profile.html", context=context)
+
