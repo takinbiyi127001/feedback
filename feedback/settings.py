9a8f0bf2007f93e85b68a1f52e5491bba52fab87
takinbiyi127001 <51786708+takinbiyi127001@users.noreply.github.com>
2022-03-15 15:57:43 +1000

    Added a new profile app and settings

--- feedback/settings.py
+++ feedback/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'reviews.apps.ReviewsConfig',
     'profiles.apps.ProfilesConfig',
+    'reviews.apps.ReviewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
---
+++ profiles/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponseRedirect
+
+
+# Create your views here.
+
+class CreateProfileView(View):
+    def get(self, request):
+        return render(request, "profiles/create_profile.html")
+
+    # Search django upload files for reference
+    def post(self, request):
+        print(request.FILES['image'])
+        return HttpResponseRedirect("/profiles")
